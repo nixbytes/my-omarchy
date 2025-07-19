1239f39bde3f4edefe769b10e480029e12d0db53
x0datasmith <real8686@gmail.com>
2025-07-18 20:08:24 -0400

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -13,13 +13,13 @@ echo -e "\n$ascii_art\n"
 pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
 
 echo -e "\nCloning Omarchy..."
-rm -rf ~/.local/share/omarchy/
-git clone https://github.com/nixbytes/my-omarchy.git ~/.local/share/omarchy >/dev/null
+rm -rf ~/.local/share/my-omarchy/
+git clone https://github.com/nixbytes/my-omarchy.git ~/.local/share/my-omarchy >/dev/null
 
 # Use custom branch if instructed
 if [[ -n "$OMARCHY_REF" ]]; then
   echo -e "\eUsing branch: $OMARCHY_REF"
-  cd ~/.local/share/omarchy
+  cd ~/.local/share/my-omarchy
   git fetch origin "${OMARCHY_REF}" && git checkout "${OMARCHY_REF}"
   cd -
 fi
